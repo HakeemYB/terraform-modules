51e3afe9eb2a465bb1e3fdcfa8cf0b6f9ae710ff
HakeemYB <baighakeemyar.cloudlem@gmail.com>
2023-03-15 01:07:47 +0500

    updates

--- eks/main.tf
+++ eks/main.tf
@@ -21,10 +21,7 @@ resource "aws_eks_cluster" "this" {
   name     = var.name
   role_arn = var.role_arn
   vpc_config {
-    subnet_ids = [
-      var.private_subnet_ids,
-      var.public_subnet_ids
-    ]
+    subnet_ids = var.private_subnet_ids
     
   }
 }
