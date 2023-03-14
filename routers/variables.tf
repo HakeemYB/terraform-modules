20319ef31ca3cb0a296bd35ad4612dee9389dca4
HakeemYB <baighakeemyar.cloudlem@gmail.com>
2023-03-15 00:39:56 +0500

    updates

--- routers/variables.tf
+++ routers/variables.tf
@@ -1,9 +1,9 @@
 variable "vpc_id" {
   type    = string
-  default = ""
+  default = "vpc-0062a907480790450"
 }
 
 variable "public_subnet_ids" {
   type    = list(string)
-  default = []
+  default = ["subnet-04a3427b99b0a960e", "subnet-0aceeb937602c4029"]
 }
