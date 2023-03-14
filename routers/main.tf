95413aab99930f714cbbaeb9186ae209d786fc01
HakeemYB <baighakeemyar.cloudlem@gmail.com>
2023-03-15 01:05:56 +0500

    updates

--- routers/main.tf
+++ routers/main.tf
@@ -6,7 +6,7 @@ resource "aws_route_table" "public" {
   vpc_id = var.vpc_id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "10.0.0.0/16"
     gateway_id = aws_internet_gateway.this.id
   }
 }
