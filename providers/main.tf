9fefe8c57007975ed9359aac7bdb7ae25807169b
HakeemYB <baighakeemyar.cloudlem@gmail.com>
2023-03-14 23:27:31 +0500

    updated providers

--- providers/main.tf
+++ providers/main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
+  
   region = var.region
 }
 
-output "aws_region" {
-  value = provider.aws.region
-}
+
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,14 +1,17 @@
-variable "cidr_block" {
-  type    = string
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC."
   default = "10.0.0.0/16"
 }
 
-variable "public_cidr_blocks" {
-  type    = list(string)
+variable "public_subnet_cidr_blocks" {
+  description = "CIDR blocks for the public subnets."
+  type        = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "private_cidr_blocks" {
-  type    = list(string)
+variable "private_subnet_cidr_blocks" {
+  description = "CIDR blocks for the private subnets."
+  type        = list(string)
   default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
