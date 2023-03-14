b34a4214cfa33f1c3b2247408eef94585aefe5f7
HakeemYB <baighakeemyar.cloudlem@gmail.com>
2023-03-15 01:19:36 +0500

    updates

--- iam/main.tf
+++ iam/main.tf
@@ -68,10 +68,6 @@ resource "aws_iam_policy" "this" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "this" {
-  policy_arn = aws_iam_policy.this.arn
-  role       = aws_iam_role.this.name
-}
 
 
 resource "aws_iam_role_policy_attachment" "this" {
