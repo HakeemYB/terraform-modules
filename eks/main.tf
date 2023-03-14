69189503613c1d6eb33dac0e395bc313eb1a6b7e
HakeemYB <baighakeemyar.cloudlem@gmail.com>
2023-03-15 01:17:17 +0500

    updates

--- eks/main.tf
+++ eks/main.tf
@@ -1,28 +1,9 @@
-resource "aws_iam_role" "demo" {
-  name = var.name
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
 resource "aws_eks_cluster" "this" {
   name     = var.name
   role_arn = var.role_arn
   vpc_config {
-    subnet_ids = var.private_subnet_ids
     
+    subnet_ids = var.private_subnet_ids
   }
 }
 
